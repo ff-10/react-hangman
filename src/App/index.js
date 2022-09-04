d199275d7ea1de5be07d59798416e961106c6cbf
Fatali Fataliyev <fataliyev999@gmail.com>
2022-09-04 05:30:34 +0400

    Hangman component created & images added

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,8 @@
 import { Component } from "react";
+import Hangman from "../Hangman";
 
 export default class App extends Component {
     render() {
-        return (<div>Hangman Game</div>);
+        return <Hangman/>;
     }
 }
---
+++ src/Hangman/index.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import style from './style.module.css';
+
+export default class Hangman extends Component {
+    render() {
+        return (<div>Hangman Game</div>);
+    }
+}
